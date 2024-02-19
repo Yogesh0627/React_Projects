73c515feca6f488c08faf2a2e7f9de4dca6c0f3a
Yogesh <chauhanyogesh950@gmail.com>
2024-02-19 17:10:13 +0530

    completed

--- oneCard/src/Components/Blog_Component.jsx
+++ oneCard/src/Components/Blog_Component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Blog_Component = ({ imgLink, date, query }) => {
   // console.log('bg',imgLink)
   return (
-    <div className="w-fit h-fit rounded-xl  ">
+    <div className="w-full md:w-fit h-fit rounded-xl  ">
       <img className="rounded-xl shadow-md w-full " src={imgLink} alt="" />
       <div className="w-[96%] py-4 ">
         <p className="text-sm text-[#696969]">{date}</p>
